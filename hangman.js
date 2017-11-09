ae909eb625c850606c827191116ac977e0fbdd02
Juliette Rapala <jrapala@gmail.com>
2017-11-08 18:39:26 -0600

    Adding ability to replay game.

--- hangman.js
+++ hangman.js
@@ -17,6 +17,8 @@
 
 		// NPM packages & exports
 		var inquirer = require('inquirer');
+		var chalk = require('chalk');
+		var isLetter = require('is-letter');
 		var WordConstructor = require('./wordConstructor.js');
 
 	// Functions
@@ -54,42 +56,65 @@
 				}]).then(function(response) {
 					// Take letter passed in by user
 					var guessedLetter = response.userLetter;
-					// Add letter to list of guessed letters
-					self.guessedLetters.push(guessedLetter);
-					// Check if letter is in word
-					var letterFound = self.currentWord.checkLetter(guessedLetter);
-					// Check if word was found 
-					var wordFound = self.currentWord.checkIfWordFound();
-					if (wordFound) {
-						console.log("You won!");
+					// Validate letter
+					if (!isLetter(guessedLetter)) {
+						console.log("\nYou did not choose a letter. Please try again!\n");
+						self.promptUser();
 					} else {
-						// Refresh gameboard	
-						self.currentWord.createGameboard();
-						// Check if a letter was guessed correctly	
-						if (letterFound) {
-							console.log("\nCORRECT!!!\n");
+						// Add letter to list of guessed letters
+						self.guessedLetters.push(guessedLetter);
+						// Check if letter is in word
+						var letterFound = self.currentWord.checkLetter(guessedLetter);
+						// Check if word was found 
+						var wordFound = self.currentWord.checkIfWordFound();
+						if (wordFound) {
+							console.log("\nCORRECT!!!");
+							console.log("You won!");
+							console.log(`The word was "${self.currentWord.word}"!\n`);
+							self.playAgain();
 						} else {
-							console.log("\nINCORRECT!!!\n");
-							self.guessesRemaining--;
-							// Display remaining guesses if there are more than 0
-							if (self.guessesRemaining === 1) {
-								console.log(`${self.guessesRemaining} guess remaining!!!\n`);
-							} else if (self.guessesRemaining > 0) {
-								console.log(`${self.guessesRemaining} guesses remaining!!!\n`);
-							};
-						}
-						// If the user has guesses remaining, prompt for another guess
-						if (self.guessesRemaining > 0) {
-							self.promptUser();
-						// If the user has run out of guesses, end game and display word.
-						} else if (self.guessesRemaining === 0) {
-							console.log(`Sorry, you lose! The word was "${self.currentWord.word}"\n`);
-						}
-					};
-
+							// Refresh gameboard	
+							self.currentWord.createGameboard();
+							// Check if a letter was guessed correctly	
+							if (letterFound) {
+								console.log("\nCORRECT!!!\n");
+							} else {
+								console.log("\nINCORRECT!!!\n");
+								self.guessesRemaining--;
+								// Display remaining guesses if there are more than 0
+								if (self.guessesRemaining === 1) {
+									console.log(`${self.guessesRemaining} guess remaining!!!\n`);
+								} else if (self.guessesRemaining > 0) {
+									console.log(`${self.guessesRemaining} guesses remaining!!!\n`);
+								};
+							}
+							// If the user has guesses remaining, prompt for another guess
+							if (self.guessesRemaining > 0) {
+								self.promptUser();
+							// If the user has run out of guesses, end game and display word.
+							} else if (self.guessesRemaining === 0) {
+								console.log(`Sorry, you lose! The word was "${self.currentWord.word}"\n`);
+								self.playAgain();
+							}
+						};
+					}
 				});
 
 				// console.log(this.currentWord.letters);
+			},
+			playAgain : function() {
+				var self = this;
+				inquirer.prompt([{
+					name: "userChoice",
+      				type: "input",
+      				message: "Would you like to play again? (Y/N)"
+				}]).then(function(response) {
+					if (response.userChoice.toUpperCase() === "Y") {
+						self.startGame();
+					} else {
+						console.log("\nThanks for playing!\n");
+					}
+				});
 			}
 		};
 
